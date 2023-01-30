989017cd4cfc29c22a5b57dd753e06737009f93b
Colin Macrae <csmacrae@gmail.com>
2023-01-30 18:28:59 +0000

    completed javascript-primitives-and-variables exercise

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,14 @@
+var fullName = 'Colin Macrae';
+var isCool = false;
+var totalPets = 0;
+console.log('Full name:', fullName);
+console.log('Is cool?:', isCool);
+console.log('Number of pets:', totalPets);
+
+var unknown = null;
+console.log('null:', unknown);
+console.log('null type:', typeof unknown);
+
+var never;
+console.log('never:', never);
+console.log('never type:', typeof never);
