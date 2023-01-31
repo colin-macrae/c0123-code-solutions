3e2a3ae7a5596d727a6065de51343a6a9dde786f
Colin Macrae <csmacrae@gmail.com>
2023-01-31 19:52:17 +0000

    completed javascript-functions exercise

---
+++ javascript-functions/main.js
@@ -0,0 +1,19 @@
+function convertMinutesToSeconds(minutes) {
+  return minutes * 60;
+}
+console.log('Number of seconds:', convertMinutesToSeconds(1));
+
+function greet(name) {
+  return 'Hello ' + name + '!';
+}
+console.log('Greeting:', greet('Mike'));
+
+function getArea(width, height) {
+  return width * height;
+}
+console.log('Area:', getArea(17, 42));
+
+function getFirstName(person) {
+  return person.firstName;
+}
+console.log('First name:', getFirstName({ firstName: 'Colin', lastName: 'Macrae' }));
