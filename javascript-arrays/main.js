0feca651398ed8d01ab510415cf610b7c658424a
Colin Macrae <csmacrae@gmail.com>
2023-01-31 01:01:52 +0000

    completed javascript-arrays exercise

---
+++ javascript-arrays/main.js
@@ -0,0 +1,18 @@
+var colors = ['red ', 'white ', 'blue'];
+console.log('value of colors:', colors[0]);
+console.log('value of colors:', colors[1]);
+console.log('value of colors:', colors[2]);
+
+console.log('am colors:', 'America is ' + colors[0] + colors[1] + 'and ' + colors[2] + '.');
+
+colors[2] = 'green';
+console.log('Mexico is:', 'Mexico is ' + colors[0] + colors[1] + 'and ' + colors[2] + '.');
+
+var students = ['Lam', 'Darren', 'Colin', 'Bishon'];
+var numberOfStudents = students.length;
+console.log('Number of students:', 'There are ' + numberOfStudents + ' students in the class.');
+var lastIndex = [numberOfStudents - 1];
+var lastStudent = students[lastIndex];
+console.log('Last student is:', lastStudent);
+console.log('Last student is:', 'The last student in the array is ' + lastStudent);
+console.log('Student array console logged:', students);
