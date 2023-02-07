08b420a290903be3c7cd482c1b8c08b918869d6e
Colin Macrae <csmacrae@gmail.com>
2023-02-07 00:17:18 +0000

    completed dom-manipulation exercise

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -8,15 +8,15 @@ $hotButton.addEventListener('click',
     // console.log($timesClicked);
     $clickCount.textContent = 'Clicks:' + $timesClicked;
     // document.getElementsByClassName('.cold');
-    if ($timesClicked <= 4) {
+    if ($timesClicked < 4) {
       $hotButton.className = 'hot-button cold';
-    } else if ($timesClicked <= 7) {
+    } else if ($timesClicked < 7) {
       $hotButton.className = 'hot-button cool';
-    } else if ($timesClicked <= 10) {
+    } else if ($timesClicked < 10) {
       $hotButton.className = 'hot-button tepid';
-    } else if ($timesClicked <= 13) {
+    } else if ($timesClicked < 13) {
       $hotButton.className = 'hot-button warm';
-    } else if ($timesClicked <= 16) {
+    } else if ($timesClicked < 16) {
       $hotButton.className = 'hot-button hot';
     } else {
       $hotButton.className = 'hot-button nuclear';
