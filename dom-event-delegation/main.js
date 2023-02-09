6055d3807d2878a520be2894a077d04ff5684793
Colin Macrae <csmacrae@gmail.com>
2023-02-09 18:31:00 +0000

    completed dom-event-delegation exercise

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -6,21 +6,10 @@ function taskListCallback(e) {
   console.log('event target:', e.target);
   console.log('event tagname:', e.target.tagName);
   if (e.target && e.target.nodeName === 'BUTTON') {
+
     console.log(taskList.closest('.task-list-item'));
     taskListItem.remove();
   }
 }
 
-// var openModalButton = document.querySelector('.open-modal-button');
-// var noButton = document.querySelector('.no-button');
-// var displayOff = document.querySelector('.display-off');
-
-// openModalButton.addEventListener('click', openModalFunction);
-// function openModalFunction(e) {
-//   displayOff.className = 'container-2';
-// }
-
-// noButton.addEventListener('click', closeModalFunction);
-// function closeModalFunction(e) {
-//   displayOff.className = 'container-2 display-off';
-// }
+// or >> if (e.target.tagName === 'BUTTON')
