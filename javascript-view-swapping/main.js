b2a1994c475576b6c1ff34252a91d22f30ba1500
Colin Macrae <csmacrae@gmail.com>
2023-02-10 04:17:47 +0000

    completed javascript-view-swapping exercise

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -1,52 +1,27 @@
-// var $tabContainer = document.querySelector('.tab-container');
-// var $tabs = document.querySelectorAll('.tab');
-// var $views = document.querySelectorAll('.view');
+var $tabContainer = document.querySelector('.tab-container');
+var $tabs = document.querySelectorAll('.tab');
+var $views = document.querySelectorAll('.view');
 
-// $tabContainer.addEventListener('click', tabSwitcher);
+$tabContainer.addEventListener('click', tabSwitcher);
 
-// function tabSwitcher(e) {
-//   if (e.target.matches('.tab')) {
-//     for (var i = 0; i < $tabs.length; i++) {
-//       if ($tabs[i] === e.target) {
-//         $tabs[i].className = 'tab active';
-//       } else if ($tabs[i] !== e.target) {
-//         $tabs[i].className = 'tab';
-//       }
-//     }
+function tabSwitcher(e) {
+  if (e.target.matches('.tab')) {
+    for (var i = 0; i < $tabs.length; i++) {
+      if ($tabs[i] === e.target) {
+        $tabs[i].className = 'tab active';
+      } else if ($tabs[i] !== e.target) {
+        $tabs[i].className = 'tab';
+      }
+    }
 
-//     if (event.target) {}
-//   }
-// }
+    var $label = e.target.getAttribute('data-view');
 
-// openModalButton.addEventListener('click', openModalFunction);
-// function openModalFunction(e) {
-//   displayOff.className = 'container-2';
-// }
-
-// var taskList = document.querySelector('.task-list');
-// var taskListItem = document.querySelector('.task-list-item');
-
-// taskList.addEventListener('click', taskListCallback);
-// function taskListCallback(e) {
-//   console.log('event target:', e.target);
-//   console.log('event tagname:', e.target.tagName);
-//   if (e.target && e.target.nodeName === 'BUTTON') {
-
-//     console.log(taskList.closest('.task-list-item'));
-//     taskListItem.remove();
-//   }
-// }
-
-// var openModalButton = document.querySelector('.open-modal-button');
-// var noButton = document.querySelector('.no-button');
-// var displayOff = document.querySelector('.display-off');
-
-// openModalButton.addEventListener('click', openModalFunction);
-// function openModalFunction(e) {
-//   displayOff.className = 'container-2';
-// }
-
-// noButton.addEventListener('click', closeModalFunction);
-// function closeModalFunction(e) {
-//   displayOff.className = 'container-2 display-off';
-// }
+    for (var j = 0; j < $views.length; j++) {
+      if ($views[j].getAttribute('data-view') !== $label) {
+        $views[j].className = 'view hidden';
+      } else {
+        $views[j].className = 'view';
+      }
+    }
+  }
+}
