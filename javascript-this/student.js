99fc00402a31fef32716691d7791cb83ddb83a05
Colin Macrae <csmacrae@gmail.com>
2023-02-20 21:35:33 +0000

    completed javascript-this exercise

--- javascript-this/student.js
+++ javascript-this/student.js
@@ -4,10 +4,11 @@ var student = {
   lastName: 'macrae',
   subject: 'JS',
   getFullName: function () {
+    // console.log(this.firstName);
     return this.firstName + ' ' + this.lastName;
   },
   getIntroduction: function () {
+    // console.log(this.getFullName());
     return 'Hello, my name is' + ' ' + this.firstName + ' ' + this.lastName + ' ' + 'and I am studying' + ' ' + this.subject + '.';
   }
-
 };
