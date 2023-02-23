f7b1f499f86b8b4a3f9de56f448c17ebc7f871bf
Colin Macrae <csmacrae@gmail.com>
2023-02-23 01:46:37 +0000

    in progress javascript-timers exercise

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -1,27 +1,20 @@
 // var interval = setInterval(countdown, 1000);
-var ticker = 1;
+var ticker = 4;
 var countDownId = null;
+var $countDown = document.querySelector('.countdown-display');
+
 function countDown() {
-  if (ticker === 1) {
-    document.querySelector('.four').textContent = '4';
-    ticker += 1;
-  } else if (ticker === 2) {
-    document.querySelector('.four').textContent = '3';
-    ticker += 1;
-  } else if (ticker === 3) {
-    document.querySelector('.four').textContent = '2';
-    ticker += 1;
-  } else if (ticker === 4) {
-    document.querySelector('.four').textContent = '1';
-    ticker += 1;
-  } else if (ticker === 5) {
-    document.querySelector('.four').textContent = '~~Earth Beloowwww Usssss~~';
+  ticker--;
+  if (ticker > 0) {
+    $countDown.textContent = ticker;
+  } else {
+    $countDown.textContent = '~~Earth Beloowwww Usssss~~';
+    clearInterval(countDownId);
   }
-  clearInterval(countDownId);
 }
 countDownId = setInterval(countDown, 1000);
 
-// // THE CODE BELOW FUNCTIONS PERFECTLY.  THE CODE ABOVE IS ATTEMPTING TO FOLLOW SHAWN'S WAY (SEE PICS & CODE BELOW)
+// // CODE BELOW IS MY ORIGINAL METHOD (REJECTED BUT FUNCTIONAL), AND WITHOUT clearInterval();
 // var interval = setInterval(countdown, 1000);
 // var ticker = 1;
 // function countdown() {
@@ -40,5 +33,4 @@ countDownId = setInterval(countDown, 1000);
 //   } else if (ticker === 5) {
 //     document.querySelector('.four').textContent = '~~Earth Beloowwww Usssss~~';
 //   }
-//   clearInterval(interval);
 // }
