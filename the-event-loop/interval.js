a5721f5e8d896656e41f82f8f3fcb76094b8886f
Colin Macrae <csmacrae@gmail.com>
2023-03-15 18:12:14 +0000

    completed the-event-loop exercise

--- the-event-loop/interval.js
+++ the-event-loop/interval.js
@@ -1,22 +1,12 @@
 let currentCount = 3;
 
 setInterval(
-  function () {
-    if (currentCount === 3) {
-      console.log('3');
-    }
-
-    if (currentCount === 2) {
-      console.log('2');
-    }
-
-    if (currentCount === 1) {
-      console.log('1');
-    }
-
-    if (currentCount === 0) {
+  () => {
+    if (currentCount > 0) {
+      console.log(currentCount);
+    } else if (currentCount === 0) {
       console.log('Blast off!');
-    }
+    } else return;
     currentCount -= 1;
   }
   , 1000);
