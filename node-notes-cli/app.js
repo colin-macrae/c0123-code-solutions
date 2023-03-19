4bdc3d02fee7ed377e04e99bf67c8ccadffd464d
Colin Macrae <csmacrae@gmail.com>
2023-03-19 04:59:22 +0000

    completed node-notes-cli exercise

---
+++ node-notes-cli/app.js
@@ -0,0 +1,60 @@
+import * as fs from 'node:fs';
+import * as fsP from 'node:fs/promises';
+
+const parsedData = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+const request = process.argv[2];
+
+handler(request);
+function handler(action) {
+  if (request === 'read') {
+    for (const property in parsedData.notes) {
+      console.log(`${property}: ${parsedData.notes[property]}`);
+    }
+  } else if (request === 'create') {
+    parsedData.notes[parsedData.nextId] = process.argv[3];
+    parsedData.nextId += 1;
+    fsP.writeFile('data.json', JSON.stringify(parsedData, null, 2));
+  } else if (request === 'update') {
+    const id = process.argv[3];
+    if (parsedData.notes[id]) {
+      parsedData.notes[id] = process.argv[4];
+      fsP.writeFile('data.json', JSON.stringify(parsedData, null, 2));
+    } else {
+      console.log('That note ID currently does not exist');
+    }
+  } else if (request === 'delete') {
+    const id = process.argv[3];
+    if (parsedData.notes[id]) {
+      delete parsedData.notes[id];
+      fsP.writeFile('data.json', JSON.stringify(parsedData, null, 2));
+    } else {
+      console.log('That note ID currently does not exist');
+    }
+  } else {
+    console.log('Invalid request.  Please type "read" or "create" or "update or "delete"');
+  }
+}
+
+// import { readFile } from 'node:fs/promises';
+
+// try {
+//   const filePath = new URL('test.txt', import.meta.url);
+//   const contents = await readFile(filePath, { encoding: 'utf8' });
+//   console.log(contents);
+// } catch (err) {
+//   console.error(err.message);
+// }
+
+// import * as fs from 'node:fs/promises';
+
+// try {
+//   const contents = await fs.readFile(process.argv[2], { encoding: 'utf8' });
+//   console.log(contents);
+// } catch (err) {
+//   console.error(err.message);
+// }
+
+// // import * as fs from 'node:fs/promises';
+
+// // fs.writeFile('rata.json', JSON.stringify(process.argv[2]))
+// //   .catch((err) => console.error(err));
