439af53d8e325bc34f69420ed3f9b845b630242e
Colin Macrae <csmacrae@gmail.com>
2023-03-30 17:01:17 +0000

    add notes to react-rendering-lists

--- react-rendering-lists/my-app/src/PokemonList.js
+++ react-rendering-lists/my-app/src/PokemonList.js
@@ -6,3 +6,11 @@ export default function PokemonList({ pokedexArray }) {
     <ul>{listItems}</ul>
   )
 }
+
+
+// alternatively you can embed it directly, without the listItems variable
+// export default function PokemonList({ pokedexArray }) {
+//   return (
+//     pokedexArray.map(character => <li>{character.name}</li>)
+//   )
+// }
