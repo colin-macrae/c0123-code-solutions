c224bc8112285706ee41d1a8dc8f69950039636e
Colin Macrae <csmacrae@gmail.com>
2023-04-03 21:58:22 +0000

    completed express-static exercise

---
+++ express-static/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(8080, () => {
+  console.log('Express server listening on port 8080');
+});
+
+// console.log(req.method);
+// console.log(res.send('hello world!'));
+
+// app.use((req, res) => {
+
+// });
