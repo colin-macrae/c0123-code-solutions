385ef49922751ba328c91bc1e9bfd8fc17c48505
Colin Macrae <csmacrae@gmail.com>
2023-04-06 14:45:38 +0000

    in progress

---
+++ es6-classes/shape.js
@@ -0,0 +1,15 @@
+/* exported Shape */
+class Shape {
+  constructor(area, circumference) {
+    this.area = area;
+    this.circumference = circumference;
+  }
+
+  print() {
+    console.log(`The area is ${this.area} and the circumference is ${this.circumference}`);
+  }
+}
+
+new Shape 
+
+// the cirle takes one parameter, square takes width, and ..
