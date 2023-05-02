cc218b75b6bcd25f595a3496e8a3ecaca4ed6744
Colin Macrae <csmacrae@gmail.com>
2023-05-02 02:56:50 +0000

    update

--- react-props-and-expressions/my-app/src/CustomButton.js
+++ react-props-and-expressions/my-app/src/CustomButton.js
@@ -1,5 +1,5 @@
 export default function CustomButton({text, color}) {
   return (
-      <button style={{backgroundColor:color}}>{text}</button>
+      <button style={{backgroundColor:color, height: 100}}>{text}</button>
   )
 }
